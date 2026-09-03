5f708fa1d75fec6be9531daf5eb913f48b11db6d
GANESH PEDDINTI <ganeshpeddinti40@gmail.com>
2026-09-03 21:37:03 +0530

    Fix production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "https://college-sms-react-postgresql.onrender.com"
 });
 
 export default API;
